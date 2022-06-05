5222f7ac06a16331736a0cf5403612934f7af1ef
Rabii Kahlaoui <rabii.kahlaouiii@gmail.com>
2022-06-05 19:55:55 +0100

    Create header and Trending Components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
 import Header from '../components/Header'
+import Trending from '../components/trending'
 
 const Home: NextPage = () => {
   return (
     <div className='min-h-screen'>
       <Header />
       <div className='mt-10'/>
+      <Trending />
       <div className='mt-20'/>
     </div>
   )
